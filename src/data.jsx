512471c9ae0ef17909aff7e9e66cee67f84c87ce
Lucas <lucasadelmo2@gmail.com>
2023-09-17 21:58:49 -0300

    final commit

---
+++ src/data.jsx
@@ -0,0 +1,45 @@
+export const projects = [
+    {
+      title: "Medication Management",
+      subtitle: "React",
+      description:
+        "This project is a system for medication management",
+      image: "./project-1.png",
+      link: "https://medication-management-gray.vercel.app/",
+    },
+    {
+      title: "DevInPharma",
+      subtitle: "Javacript, CSS/HTML and Bootsrap",
+      description:
+        "This project simulates a pharmacy website",
+      image: "./project-2.png",
+      link: "https://e-commerce-farmacia.vercel.app/",
+    },
+    {
+      title: "Organo",
+      subtitle: "React",
+      description:
+        "This project is a builder organization chart of a company",
+      image: "./project-3.png",
+      link: "https://organo-react-ivory.vercel.app/",
+    },
+    {
+      title: "Tools",
+      subtitle: "Javacript, CSS/HTML and Bootsrap",
+      description:
+        "This is a math tools app",
+      image: "./project-4.png",
+      link: "https://m1-s04-js-exercises.vercel.app/",
+    },
+  ];
+
+export const skills = [
+  "Node.js",
+  "Javascript",
+  "Java",
+  "HTML/CSS",
+  "React",
+  "SQL"
+
+
+]
